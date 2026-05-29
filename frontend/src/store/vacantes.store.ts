e0e60a913f916a75b1ed13ea2258ff2123574b76
scarletvargas <scarletvf47@gmail.com>
2026-05-29 17:45:21 -0400

    actualiizacion del store de vacantes

--- frontend/src/store/vacantes.store.ts
+++ frontend/src/store/vacantes.store.ts
@@ -1,11 +1,18 @@
 import { create } from "zustand";
 import * as vacantesService from "@/services/vacantes.service";
 import { VacanteFormData } from "@/schemas/vacante.schema";
+import { Vacante } from "../../types";
 
 interface VacantesStore {
   vacantes: Vacante[];
+  vacanteDetalle: Vacante | null;
+  error: string | null;
   loading: boolean;
+  loadingDetalle: boolean;
   getVacantes: () => Promise<void>;
+  getVacanteById: (id: number) => Promise<void>;
+
+  clearVacanteDetalle: () => void;
   createVacante: (data: VacanteFormData) => Promise<void>;
   updateVacante: (id: number, data: VacanteFormData, estado: "abierta" | "cerrada") => Promise<void>;
   deleteVacante: (id: number) => Promise<void>;
@@ -13,19 +20,61 @@ interface VacantesStore {
 
 export const useVacantesStore = create<VacantesStore>((set, get) => ({
   vacantes: [],
+  vacanteDetalle: null,
+
   loading: false,
+  loadingDetalle: false,
+
+  error: null,
+
 
   getVacantes: async () => {
-    set({ loading: true });
+    set({ loading: true, error: null });
     try {
       const data = await vacantesService.getMisVacantes();
       set({ vacantes: data });
     } catch (error) {
-      console.error(error);
+      set({
+        error:
+          error instanceof Error
+            ? error.message
+            : "Error obteniendo vacantes",
+      });
     } finally {
       set({ loading: false });
     }
   },
+  getVacanteById: async (id) => {
+    set({
+      loadingDetalle: true,
+      error: null,
+    });
+
+    try {
+      const data =
+        await vacantesService.getVacanteById(id);
+
+      set({
+        vacanteDetalle: data,
+      });
+    } catch (error) {
+      set({
+        error:
+          error instanceof Error
+            ? error.message
+            : "Error obteniendo vacante",
+      });
+    } finally {
+      set({
+        loadingDetalle: false,
+      });
+    }
+  },
+
+  clearVacanteDetalle: () =>
+    set({
+      vacanteDetalle: null,
+    }),
   createVacante: async (data) => {
     try {
       const nueva = await vacantesService.crearVacante(data);
@@ -43,8 +92,10 @@ export const useVacantesStore = create<VacantesStore>((set, get) => ({
       set((state) => ({
         vacantes: state.vacantes.map((v) =>
           v.id === id ? updated : v
-        ),
+        ), vacanteDetalle: updated,
       }));
+
+
     } catch (error) {
       console.error("Error actualizando vacante", error);
     }
